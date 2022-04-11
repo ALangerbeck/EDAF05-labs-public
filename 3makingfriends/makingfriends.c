7236166f32c4b0b453813cfed3fbfa16f7f2fdcc
Alfred Langerbeck <98alla02@gmail.com>
2022-04-11 15:55:13 +0200

    began input for lab 3

--- 3makingfriends/makingfriends.c
+++ 3makingfriends/makingfriends.c
@@ -6,7 +6,7 @@
 #include <string.h>
 #include <unistd.h>
 
-#define PRINT 0 /* enable/disable prints. */
+#define PRINT 1 /* enable/disable prints. */
 #define WORD_LENGTH 5
 
 #if PRINT
@@ -21,30 +21,49 @@
 
 static char *progname;
 
-typedef struct list list;
+typedef struct arc_list arc_list;
 typedef struct node node;
-typedef struct queue queue;
+typedef struct arc_queue arc_queue;
+typedef struct arc arc;
+typedef struct graph graph;
 
-struct queue
+struct  arc
 {
-    list *first;
-    list *last;
+    node *u;
+    node *v;
+    int index;
+    int minutes;
 };
 
-struct list
+
+struct arc_queue
 {
-    node *node;
-    list *next;
+    arc_list *first;
+    arc_list *last;
+};
+
+
+
+struct arc_list
+{
+    node *arc;
+    arc_list *next;
 };
 
 struct node
 {
     int index;
-    char word[WORD_LENGTH];
-    list *adjacency;
-    int visited;
-    node *pred;
+    arc_list *arc;
+    node *next;
 };
+struct graph
+{
+    int n;       /* nodes.			*/
+    int p;      //number of arcs
+    node* nodes; //array of nodes
+    arc* arcs;  //arra of arcs
+};
+
 
 static int next_int()
 {
@@ -56,12 +75,55 @@ static int next_int()
     return x;
 }
 
+
+graph* initGraph(int n, int p){
+    graph *g = (graph *)malloc(sizeof(graph));
+    node *nodes = (node *)malloc(n*sizeof(node));
+    arc *arcs = (arc *)malloc(p*sizeof(arc));
+
+    
+    g->arcs = arcs;
+    g->nodes = nodes;
+
+    if (PRINT)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            g->nodes[i].index = i;
+        }
+        
+    }
+    
+    
+
+
+    int temp;
+    for (size_t i = 0; p < n; i++)
+    {   
+        g->arcs[i].index = i;
+        scanf("%d",temp);
+        g->arcs[i].u = &g->nodes[temp];
+        g->nodes[temp].index = temp;
+    }
+    
+
+}
+
 int main(int argc, char *argv[])
 {
     FILE *in; /* input file set to stdin	*/
-    int n;    /* number of words.		*/
-    int q;    // Number of queries
+    int n;    /* number of people.		*/
+    int p;    // Number of pairs
 
     progname = argv[0]; /* name is a string in argv[0]. */
     in = stdin;         /* same as System.in in Java.	*/
+    
+    scanf("%1d",&n);
+    scanf("%1d",&p);
+    
+    pr("Number of people: %d\n",n);
+    pr("Number of pairs: %d\n",p);
+    
+    graph* g = initGraph(n,p);
+    
 }
