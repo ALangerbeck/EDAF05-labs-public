e45803007a8e4b06e35cfcc9180e86704f759e4b
ALangerbeck <98alla02@gmail.com>
2022-05-09 21:53:16 +0200

    more on lab 4

--- 4closestpair/closest.c
+++ 4closestpair/closest.c
@@ -1,10 +1,10 @@
-#include <assert.h>
 #include <ctype.h>
 #include <stdarg.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <math.h>
 
 #define PRINT 1 /* enable/disable prints. */
 
@@ -28,17 +28,7 @@ struct point{
 
 };
 
-static int next_int()
-{
-    int x;
-    int c;
-    x = 0;
-    while (isdigit(c = getchar()))
-        x = 10 * x + c - '0';
-    return x;
-}
-
-void merge(int arr[], int l, int m, int r)
+void merge(int arr[], int l, int m, int r,int isX,point* players)
 {
     int i, j, k;
     int n1 = m - l + 1;
@@ -57,8 +47,18 @@ void merge(int arr[], int l, int m, int r)
     i = 0; // Initial index of first subarray
     j = 0; // Initial index of second subarray
     k = l; // Initial index of merged subarray
+    int leftVal;
+    int rightVal;
     while (i < n1 && j < n2) {
-        if (L[i] <= R[j]) {
+        if (isX)
+        {   
+            leftVal = players[L[i]].x_cord;
+            rightVal = players[R[i]].x_cord;
+        }else{
+            int leftVal = players[L[i]].y_cord;
+            int rightVal = players[R[i]].y_cord;
+        }
+        if (leftVal <= rightVal) {
             arr[k] = L[i];
             i++;
         }
@@ -67,7 +67,10 @@ void merge(int arr[], int l, int m, int r)
             j++;
         }
         k++;
+        
+        
     }
+  
     /* Copy the remaining elements of L[], if there
     are any */
     while (i < n1) {
@@ -84,8 +87,10 @@ void merge(int arr[], int l, int m, int r)
         k++;
     }
 }
-
-void mergeSort(int arr[], int l, int r)
+  
+/* l is for left index and r is right index of the
+sub-array of arr to be sorted */
+void mergeSort(int arr[], int l, int r , int isX, point* players)
 {
     if (l < r) {
         // Same as (l+r)/2, but avoids overflow for
@@ -93,12 +98,23 @@ void mergeSort(int arr[], int l, int r)
         int m = l + (r - l) / 2;
   
         // Sort first and second halves
-        mergeSort(arr, l, m);
-        mergeSort(arr, m + 1, r);
+        mergeSort(arr, l, m,isX,players);
+        mergeSort(arr, m + 1, r, isX,players);
   
-        merge(arr, l, m, r);
+        merge(arr, l, m, r, isX,players);
     }
 }
+static int next_int()
+{
+    int x;
+    int c;
+    x = 0;
+    while (isdigit(c = getchar()))
+        x = 10 * x + c - '0';
+    return x;
+}
+
+
 
 float distance(point point1,point point2){
     return sqrt((point1.x_cord - point2.x_cord)^2 + (point1.y_cord - point2.y_cord)^2);
@@ -111,11 +127,43 @@ point* inputPoints(int p){
     {
         players[i].index = i;
         scanf("%d",&temp);
-        players[i].x_cord;
+        players[i].x_cord = temp;
         scanf("%d",&temp);
-        players[i].y_cord;
+        players[i].y_cord = temp;
     }
     return players;
+}
+
+float bruteforce(int* px , int num_p,point* players){
+    float min = INFINITY;
+
+    for (int i = 0; i < num_p; i++)
+    {
+        for (int j = 0; j < num_p; j++)
+        {
+            float dist = distance(players[px[i]],players[px[j]]);
+            if (dist < min)
+            {
+                min = dist;
+            }
+        }
+    }
+    return min;
+    
+}
+
+float findClosest(int* px, int* py, point* players ,int num_p){
+
+    if (num_p <= 3)
+    {
+        return bruteforce(px,num_p,players);
+    }
+
+    int mid = num_p / 2;
+    
+    int
+    
+
 }
 
 int main(int argc, char *argv[])
@@ -130,9 +178,25 @@ int main(int argc, char *argv[])
 
     pr("Number of points: %d\n", p);
 
-    point *players = iniputPoints(p);
+    point *players = inputPoints(p);
 
-    mergeSort(players, 0, p - 1);
+    int* px = (int*) malloc(p*sizeof(int));
+    int* py = (int*) malloc(p*sizeof(int));
+
+    for (int i = 0; i < p; i++)
+    {
+        px[i] = i;
+        py[i] = i;
+    }
+    
+    mergeSort(px, 0, p - 1, 1,players);
+    mergeSort(py, 0, p - 1, 0,players);
+
+    for (int i = 0; i < p; i++)
+    {
+        printf("%d,\n",px[i]);
+    }
+    
 
 
     return 0;
