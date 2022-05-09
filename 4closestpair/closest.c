ffe6d4fe731424333af1f8168dfb2420d979003f
Alfred Langerbeck <98alla02@gmail.com>
2022-05-09 16:41:50 +0200

    More work on lab 4 and added python solution

--- 4closestpair/closest.c
+++ 4closestpair/closest.c
@@ -19,6 +19,14 @@
 #endif
 
 static char *progname;
+typedef struct point point;
+
+struct point{
+    int x_cord;
+    int y_cord;
+    int index;
+
+};
 
 static int next_int()
 {
@@ -30,19 +38,101 @@ static int next_int()
     return x;
 }
 
+void merge(int arr[], int l, int m, int r)
+{
+    int i, j, k;
+    int n1 = m - l + 1;
+    int n2 = r - m;
+  
+    /* create temp arrays */
+    int L[n1], R[n2];
+  
+    /* Copy data to temp arrays L[] and R[] */
+    for (i = 0; i < n1; i++)
+        L[i] = arr[l + i];
+    for (j = 0; j < n2; j++)
+        R[j] = arr[m + 1 + j];
+  
+    /* Merge the temp arrays back into arr[l..r]*/
+    i = 0; // Initial index of first subarray
+    j = 0; // Initial index of second subarray
+    k = l; // Initial index of merged subarray
+    while (i < n1 && j < n2) {
+        if (L[i] <= R[j]) {
+            arr[k] = L[i];
+            i++;
+        }
+        else {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+    /* Copy the remaining elements of L[], if there
+    are any */
+    while (i < n1) {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
+  
+    /* Copy the remaining elements of R[], if there
+    are any */
+    while (j < n2) {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+}
+
+void mergeSort(int arr[], int l, int r)
+{
+    if (l < r) {
+        // Same as (l+r)/2, but avoids overflow for
+        // large l and h
+        int m = l + (r - l) / 2;
+  
+        // Sort first and second halves
+        mergeSort(arr, l, m);
+        mergeSort(arr, m + 1, r);
+  
+        merge(arr, l, m, r);
+    }
+}
+
+float distance(point point1,point point2){
+    return sqrt((point1.x_cord - point2.x_cord)^2 + (point1.y_cord - point2.y_cord)^2);
+}
+
+point* inputPoints(int p){
+    point *players = (point*)malloc(p*sizeof(point));
+    int temp;
+    for (int i = 0; i < p; i++)
+    {
+        players[i].index = i;
+        scanf("%d",&temp);
+        players[i].x_cord;
+        scanf("%d",&temp);
+        players[i].y_cord;
+    }
+    return players;
+}
+
 int main(int argc, char *argv[])
 {
     FILE *in; /* input file set to stdin	*/
-    int n;    /* number of people.		*/
-    int p;    // Number of pairs
-
+    int p;    /* number of people.		*/
+    
     progname = argv[0]; /* name is a string in argv[0]. */
     in = stdin;         /* same as System.in in Java.	*/
 
-    scanf("%d", &n);
     scanf("%d", &p);
 
-    pr("Number of people: %d\n", n);
+    pr("Number of points: %d\n", p);
+
+    point *players = iniputPoints(p);
+
+    mergeSort(players, 0, p - 1);
 
 
     return 0;
